de9d4261e36248363e8c39abccb5dd7404d3bed2
kavyasukumar <kavya.sukumar@hotmail.com>
2014-07-15 09:57:22 -0400

    embed map using pym. new dark theme

--- www/js/graphics.js
+++ www/js/graphics.js
@@ -2,42 +2,48 @@ var mapDiv = null,
 curr = 0;
 
 var margin= {left:10,right:10,top:0,bottom:0},
-	width = 1000,
-	height = 553;
+	$w = 1000,
+	$h = 553;
 
 $(document).ready(function(){
 	$('.carousel').carousel({ interval: false});
 	$('#fakemap').click(showMap);
 	var throttledResize = _.throttle(loadGraphics, 100);
-	$(window).resize(throttledResize);	
-
+	$(window).resize(throttledResize);
+    pymChild = new pym.Child({ renderCallback: loadGraphics });
 	loadGraphics();
 });
 
 function calcLayout(){
 	var maxWidth = $(window).height()*.9/.553;
-	width= Math.min(997,$('#main').width()- margin.right -margin.left,maxWidth);
-	height= width*.553; //maintaining aspect ratio
+	$w= Math.min(997,$('#main').width()- margin.right -margin.left);
+	$h= $w*.553; //maintaining aspect ratio
 
+	var mainHeight= ($h+$('#prev').height())*1.02; //giving 2 percent margin
 	$('#main')
-	.css("min-height",height+35+'px')
-	.css("height", height+35+'px')
-	.css("min-width",width+'px')
-	.css("width",width+'px');
+	.css("min-height",mainHeight+'px')
+	.css("height", mainHeight+'px')
+	.css("min-width",$w+'px')
+	.css("width",$w+'px');
 
 	$('#thumbs')
-	.css("width",width+'px');
+	.css("width",$w+'px');
 
 	$('#img-slideshow')
-	.css("width",width+'px')
-	.css("height", height*.9+'px');
+	.css("width",$w+'px')
+	.css("height", $h-$('#fakemap').height()+'px');
+
+	if($w<480)
+	{
+		$('#desc').remove();
+	}
 }
 
 function nukeGraphics(){
 	$('#map>*').remove();
 	margin= {left:10,right:10,top:0,bottom:0},
-	width = 1000,
-	height = 553;
+	$w = 1000,
+	$h = 553;
 
 	$('#main').css("width","100%");
 }
@@ -47,38 +53,38 @@ function loadGraphics(){
 	calcLayout();
 	//mapextendt in tilemill: -125.2881,24.2069,-66.6211,49.6676
 	var xScale = d3.scale.linear()
-		.range([0,width])
+		.range([0,$w])
 		.domain([-125.2881,-66.6211]);
 
 	var yScale = d3.scale.linear()
-		.range([0,height])
+		.range([0,$h])
 		.domain([49.6676,25.5]);
 
 
 	mapDiv = d3.select('#map')
-		.style("height",height+'px')
-		.style("width",width+'px');
+		.style("height",$h+'px')
+		.style("width",$w+'px');
 
 	//load data and draw park points	
 	d3.json("./js/data/parks.json", function(error, data) {
 		parkData =data.parks;
 
-		var points = mapDiv.selectAll(".park")
+		var parkPoints = mapDiv.selectAll(".park")
 		.data(parkData)
 		.enter().append("circle")
+		.attr("id",function(d,i){return "park"+i;})
 		.attr("class","park")
 		.style("fill",'#ff0000')
 		.attr("r",5)
 		.attr("cx",function(d){return xScale(d.long)})
-		.attr("cy",function(d){return yScale(d.lat)+7})
-		.style("z-index",100);
+		.attr("cy",function(d){return yScale(d.lat)+7});
 
 		var labels= mapDiv.selectAll(".label")
 		.data(parkData)
 		.enter().append("text")
 		.text(function(d){return d.name;})
 		.attr("x", function(d){return xScale(d.long)+10})
-		.attr("y",function(d){return yScale(d.lat)+12});
+		.attr("y",function(d){return yScale(d.lat)+12});	
 
 		loadImgs();
 	});
@@ -92,12 +98,11 @@ function loadGraphics(){
 		.y(function(d){return yScale(d.lat)+7});
 
 		var id = 'path-'+from.id+'-'+to.id;
-		var path= mapDiv.append("path")
+		var path= mapDiv.insert("path","circle")
 		.attr("d",line([from,to]))
 		.attr("id",id)
 		.attr("stroke", "red")
-		.attr("stroke-width",2)
-		.style("z-index",0);
+		.attr("stroke-width",2);
 
 		var totalLength = path.node().getTotalLength();
 
@@ -162,7 +167,21 @@ function loadGraphics(){
 	})
 }
 
+
+function updateCurrpark()
+{
+	var text = '<span class="bold">'+parkData[curr].fullName+"</span> : "+ parkData[curr].desc
+	$('#desc').html(text);
+
+	var h = $('#desc').height();
+	$('#desc').css('bottom',h+10+'px');
+
+	d3.selectAll('.park').attr("r",3);
+	d3.select('#park'+curr).attr("r",7);
+}
+
 function loadImgs(){
+	updateCurrpark();
 	//remove all existing ones
 	$('#thumbs img').remove();
 	$('.carousel-inner .item').remove();
@@ -199,6 +218,7 @@ function showMap(){
 	if($('#map').css("display")=="none"){
 		$('#main #img-slideshow').hide(100, function() {
 			$('#map').slideDown(500);
+			$('#desc').show();
 		});
 	}
 }
@@ -206,6 +226,7 @@ function showMap(){
 function hideMap(){
 	if($('#map').css("display")!="none"){
 		$('#map').slideUp(500, showImg);
+		$('#desc').hide();
 	}
 	else{
 		showImg();
--- www/js/script.js
+++ www/js/script.js
@@ -3,14 +3,27 @@ $(document).ready(function(){
 	MedleyShiv();
 	var throttledResize = _.throttle(calcLayout, 100);
 	$(window).resize(throttledResize);
+
+	var pymparent = new pym.Parent('graphic', './img.html',{});
 });
 
 function calcLayout(){
 	// calculate cover height
-	$bgheight=$(window).height();
+	var $bgheight=$(window).height();
 	$('#coverholder').css('height', $bgheight+'px');
-	$secheight =$bgheight*.5;
-	$('.header').css('height', $secheight+'px');
+	var $secheight =$bgheight*.5;
+	$('.header.withImg').css('height', $secheight+'px');
+
+	/*var $mapWidth = $('#graphic').width();	
+	$('#map').attr("width",$mapWidth+'px')
+	.attr("height",$mapWidth*.65+'px');
+
+	$('#graphic').css('height',$mapWidth*.8+'px');*/
+
+/*
+	//force an iframe reload
+	var ifr = document.getElementById('map');
+	ifr.src = ifr.src;*/
 }
 
 function MedleyShiv(){
